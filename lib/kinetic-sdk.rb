5169c67124330b76b9af9f387785ba24622ac283
Jack Boespflug <jack.boespflug@kineticdata.com>
2018-11-12 14:36:41 -0600

    Fix logic including gems when JRuby is used.

--- lib/kinetic-sdk.rb
+++ lib/kinetic-sdk.rb
@@ -15,9 +15,8 @@ $:.unshift File.join(gemdir, 'slugify-1.0.7', 'lib')
 # add the websocket gem directories to the load path
 $:.unshift File.join(gemdir, 'kontena-websocket-client-0.1.1', 'lib')
 $:.unshift File.join(gemdir, 'websocket-extensions-0.1.3', 'lib')
-defined?(JRUBY_VERSION) ?
-  $:.unshift File.join(gemdir, 'websocket-driver-0.6.5-java', 'lib') :
-  $:.unshift File.join(gemdir, 'websocket-driver-0.6.5', 'lib')
+$:.unshift File.join(gemdir, 'websocket-driver-0.6.5-java', 'lib') if defined?(JRUBY_VERSION)
+$:.unshift File.join(gemdir, 'websocket-driver-0.6.5', 'lib') unless defined?(JRUBY_VERSION)
 
 # require parallel and slugify so they are available in scripts
 require 'parallel'
