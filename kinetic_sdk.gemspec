bb8eb9419914b0533725de62755727f9692c3a45
Jack Boespflug <jack.boespflug@kineticdata.com>
2019-05-30 15:21:27 -0500

    Bundle kinetic_sdk as a gem

---
+++ kinetic_sdk.gemspec
@@ -0,0 +1,30 @@
+
+lib = File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require "kinetic_sdk/version"
+
+Gem::Specification.new do |spec|
+  spec.name          = "kinetic_sdk"
+  spec.version       = KineticSdk::VERSION
+  spec.authors       = ["Kinetic Data"]
+  spec.email         = ["support@kineticdata.com"]
+
+  spec.summary       = %q{Ruby SDK for Kinetic Data application APIs}
+  spec.homepage      = "https://github.com/kineticdata/kinetic-sdk-rb"
+
+  spec.files         = Dir.glob("{bin,lib,gems}/**/*") + %w(README.md GettingStarted.md)
+  spec.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  spec.require_paths = ["lib"]
+
+
+  spec.add_dependency "slugify", "1.0.7"
+  spec.add_dependency "multipart-post", "2.0.0"
+  spec.add_dependency "mime-types", "3.1"
+  spec.add_dependency "parallel", "1.12.1"
+  spec.add_dependency "ruby-progressbar", "1.9.0"
+  spec.add_dependency "kontena-websocket-client", "0.1.1"
+
+  spec.add_development_dependency "bundler", "~> 1.16"
+  spec.add_development_dependency "rake", "~> 12.3.0"
+  spec.add_development_dependency "yard", "~> 0.9.12"
+end
---
+++ lib/kinetic_sdk.rb
@@ -0,0 +1,57 @@
+require 'fileutils'
+require 'json'
+require 'uri'
+require 'yaml'
+
+pwd = File.expand_path(File.dirname(__FILE__))
+gemdir = File.expand_path(File.join(pwd, '..', 'gems'))
+
+# add gem directories to load path if gem is not installed
+if Gem::Specification.find_all_by_name('mime-types').empty?
+  $:.unshift File.join(gemdir, 'mime-types-3.1', 'lib')
+end
+if Gem::Specification.find_all_by_name('mime-types-data').empty?
+  $:.unshift File.join(gemdir, 'mime-types-data-3.2016.0521', 'lib')
+end
+if Gem::Specification.find_all_by_name('multipart-post').empty?
+  $:.unshift File.join(gemdir, 'multipart-post-2.0.0', 'lib')
+end
+if Gem::Specification.find_all_by_name('parallel').empty?
+  $:.unshift File.join(gemdir, 'parallel-1.12.1', 'lib')
+end
+if Gem::Specification.find_all_by_name('ruby-progressbar').empty?
+  $:.unshift File.join(gemdir, 'ruby-progressbar-1.9.0', 'lib')
+end
+if Gem::Specification.find_all_by_name('slugify').empty?
+  $:.unshift File.join(gemdir, 'slugify-1.0.7', 'lib')
+end
+if Gem::Specification.find_all_by_name('kontena-websocket-client').empty?
+  $:.unshift File.join(gemdir, 'kontena-websocket-client-0.1.1', 'lib')
+end
+if Gem::Specification.find_all_by_name('websocket-extensions').empty?
+  $:.unshift File.join(gemdir, 'websocket-extensions-0.1.3', 'lib')
+end
+if Gem::Specification.find_all_by_name('websocket-driver').empty?
+  if defined?(JRUBY_VERSION)
+    $:.unshift File.join(gemdir, 'websocket-driver-0.6.5-java', 'lib')
+  else
+    $:.unshift File.join(gemdir, 'websocket-driver-0.6.5', 'lib')
+  end
+end
+
+require 'parallel'
+require 'slugify'
+
+require "kinetic_sdk/version"
+
+require "kinetic_sdk/utils/logger"
+require "kinetic_sdk/utils/random"
+require "kinetic_sdk/utils/kinetic-http-headers"
+require "kinetic_sdk/utils/kinetic-http-response"
+require "kinetic_sdk/utils/kinetic-http"
+
+require "kinetic_sdk/bridgehub/bridgehub-sdk"
+require "kinetic_sdk/discussions/discussions-sdk"
+require "kinetic_sdk/filehub/filehub-sdk"
+require "kinetic_sdk/request_ce/request-ce-sdk"
+require "kinetic_sdk/task/task-sdk"
