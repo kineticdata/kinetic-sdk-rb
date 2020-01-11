1d83fbe728fb4954640e7fbbb93170cddce68470
James Davies <james.davies@kineticdata.com>
2020-01-10 19:35:45 -0500

    Fixed bug with fetching jwts for discussions

--- lib/kinetic_sdk/core/lib/jwt.rb
+++ lib/kinetic_sdk/core/lib/jwt.rb
@@ -7,14 +7,14 @@ module KineticSdk
     # @param client_secret [String] the oauth client secret
     # @param headers [Hash] hash of headers to send, default is basic authentication and accept JSON content type
     # @return [KineticSdk::Utils::KineticHttpResponse] object, with +code+, +message+, +content_string+, and +content+ properties
-    def jwt_token(client_id, client_secret, headers=default_headers)
+    def jwt_token(client_id, client_secret, headers = default_headers)
       # retrieve the jwt code
       jwt_code = jwt_code(client_id, headers)
       # retrieve the jwt token
       @logger.info("Retrieving JWT authorization token")
       url = "#{@server}/app/oauth/token?grant_type=authorization_code&response_type=token&client_id=#{client_id}&code=#{jwt_code}"
       token_headers = header_accept_json.merge(header_basic_auth(client_id, client_secret))
-      response = post(url, {}, token_headers, 0)
+      response = post(url, {}, token_headers, { :max_redirects => 0 })
 
       if response.status == 401
         raise StandardError.new "#{response.message}, the oauth client id and secret are invalid."
@@ -29,18 +29,18 @@ module KineticSdk
     #
     # This method should really never need to be called externally.
     #
-    # @param client_id [String] 
+    # @param client_id [String]
     # @param headers [Hash] hash of headers to send, default is basic authentication and accept JSON content type
     # @return [KineticSdk::Utils::KineticHttpResponse] object, with +code+, +message+, +content_string+, and +content+ properties
-    def jwt_code(client_id, headers=default_headers)
+    def jwt_code(client_id, headers = default_headers)
       @logger.info("Retrieving JWT authorization code")
       url = "#{@server}/app/oauth/authorize?grant_type=authorization_code&response_type=code&client_id=#{client_id}"
-      response = post(url, {}, headers, -1)
+      response = post(url, {}, headers, { :max_redirects => -1 })
 
       if response.status == 401
-        raise StandardError.new "#{response.message}: #{response.content['error']}"
+        raise StandardError.new "#{response.message}: #{response.content["error"]}"
       elsif response.status == 302
-        location = response.headers['location']
+        location = response.headers["location"]
         if location.nil?
           raise StandardError.new "Unable to retrieve code: #{response.inspect}"
         else
@@ -50,6 +50,5 @@ module KineticSdk
         raise StandardError.new "Unable to retrieve code #{response.inspect}"
       end
     end
-
   end
 end
